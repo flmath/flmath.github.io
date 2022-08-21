e64ede348cc533d3dc1f0a0fd8340bb742617049
Mathias Green <mathias.green@teliacompany.com>
2022-08-21 18:50:05 +0200

    setup path for github repo

--- svelte.config.js
+++ svelte.config.js
@@ -9,6 +9,9 @@ const config =  {
   preprocess: preprocess(),	
  
   kit: {
+	paths: { base: dev ? '' : '/flmath.github.io',
+	},
+	appDir: 'internal',
     adapter: adapter({
       // default options are shown. On some platforms
       // these options are set automatically — see below

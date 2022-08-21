1b6596c6822899fc69e90dd71443c1bedb5d9299
Mathias Green <mathias.green@teliacompany.com>
2022-08-21 18:43:57 +0200

    nohekyll for github pages in static

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,28 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+const config =  {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  
+  preprocess: preprocess(),	
+ 
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false
+    }),
 
-	kit: {
-		adapter: adapter()
-	}
+    prerender: {
+      // This can be false if you're using a fallback (i.e. SPA mode)
+      default: true
+    }
+  }
 };
 
 export default config;

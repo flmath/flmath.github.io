2ad395c6f319d12988c23f4956bb60f1d646ab29
Mathias Green <mathias.green@teliacompany.com>
2022-08-30 08:05:08 +0200

    started transfering CV

---
+++ src/routes/cv/Interests.svelte
@@ -0,0 +1,13 @@
+
+    <div className="my_cv_container">
+        <p>
+        Most of my interest is focused around developing my skills of gathering, processing and analyzing information.
+        This is why I went to study mathematics and chose to learn software development. I also do not despise other intellectual pursuits like
+        a game of chess, <a target="_blank" rel="noopener noreferrer" href='https://en.wikipedia.org/wiki/Set_(card_game)'>set</a>,
+        an <a target="_blank" rel="noopener noreferrer" href='https://en.wikipedia.org/wiki/Rubik%27s_Cube'>interesting puzzle</a>,
+        a <a target="_blank" rel="noopener noreferrer" href='https://en.wikipedia.org/wiki/Torment:_Tides_of_Numenera'>computer game</a>,
+        a <a target="_blank" rel="noopener noreferrer" href='https://en.wikipedia.org/wiki/Snow_Crash'>book</a>,
+        or a <a target="_blank" rel="noopener noreferrer" href='https://en.wikipedia.org/wiki/Star_Trek:_Discovery'>movie</a>.
+        I have created this page to write down the notes I have produced during my pursuits.
+        </p>
+    </div>

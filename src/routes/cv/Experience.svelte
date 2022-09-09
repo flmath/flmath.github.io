2ea7caeee561744b4c09eaedd90e9e5117cd31c1
Mathias Green <mathias.green@teliacompany.com>
2022-09-09 13:26:01 +0200

    moved timeline to google component

--- src/routes/cv/Experience.svelte
+++ src/routes/cv/Experience.svelte
@@ -1,49 +1,57 @@
-<script>
-  import "$lib/assets/ExternalLibs/loader.js";
+<script lang="ts">
+  import "@google-web-components/google-chart";
 
-  google.charts.load("current", { packages: ["timeline"] });
-  google.charts.setOnLoadCallback(drawChart);
+  export let cols: {
+    label: string;
+    type: string;
+  }[] = [
+    { type: "string", label: "Term" },
+    { type: "string", label: "Name" },
+    { type: "date", label: "Start" },
+    { type: "date", label: "End" },
+  ];
 
-  function drawChart() {
-    var container = document.getElementById("chart_div");
-    var chart = new google.visualization.Timeline(container);
-    var dataTable = new google.visualization.DataTable();
-    dataTable.addColumn({ type: "string", id: "Term" });
-    dataTable.addColumn({ type: "string", id: "Name" });
-    dataTable.addColumn({ type: "date", id: "Start" });
-    dataTable.addColumn({ type: "date", id: "End" });
-
-    dataTable.addRows([
-      ["Employment", "Ericpol", new Date(2012, 2, 1), new Date(2015, 7, 31)],
-      [
-        "Project",
-        "Ericsson MTAS",
-        new Date(2012, 5, 1),
-        new Date(2013, 12, 31),
-      ],
-      ["Project", "Ericsson SBG", new Date(2014, 1, 1), new Date(2022, 3, 31)],
-      ["Employment", "DEK Q-team", new Date(2015, 8, 1), new Date(2017, 7, 1)],
-      ["Employment", "DEK UNI", new Date(2017, 7, 1), new Date(2022, 3, 31)],
-      ["Employment", "Telia", new Date(2022, 4, 1), new Date()],
-      ["Project", "ACE Core", new Date(2022, 4, 1), new Date()],
-    ]);
-
-    var options = {
-      timeline: {},
-    };
-
-    chart.draw(dataTable, options);
-  }
+  export let rows: [string, string, Date, Date][] = [
+    ["Employment", "Ericpol", new Date(2012, 2, 1), new Date(2015, 7, 31)],
+    ["Project", "Ericsson MTAS", new Date(2012, 5, 1), new Date(2013, 12, 31)],
+    ["Project", "Ericsson SBG", new Date(2014, 1, 1), new Date(2022, 3, 31)],
+    ["Employment", "DEK Q-team", new Date(2015, 8, 1), new Date(2017, 7, 1)],
+    ["Employment", "DEK UNI", new Date(2017, 7, 1), new Date(2022, 3, 31)],
+    ["Employment", "Telia", new Date(2022, 4, 1), new Date()],
+    ["Project", "ACE Core", new Date(2022, 4, 1), new Date()],
+  ];
+  export let title: String = "";
+  export let sliceVisibilityThreshold: number = 0;
 </script>
-<!-- implement update on resize -->
-<span id="chart_div" />
+
+<span class="div-chart">
+  <google-chart
+    class="chart"
+    type="timeline"
+    {cols}
+    {rows}
+    options={{
+      timeline: {},
+      title,
+      backgroundColor: "transparent",
+      titleTextStyle: { fontSize: 14, color: "#737373" },
+      sliceVisibilityThreshold,
+    }}
+  />
+</span>
 
 <style>
-  #chart_div {
+  .div-chart {
     max-width: 95vw;
     margin: 0;
     padding: 0;
-    display: block;
+    display: flex;
+    align-items: center;
+  }
+  .chart {
+    width: 100vw;
+    margin: 0;
+    padding: 0;
     align-items: center;
   }
 </style>

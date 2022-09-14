4784cf3caa94b0148b46a933398ef2c5b5525685
Mathias Green <mathias.green@teliacompany.com>
2022-09-14 11:29:56 +0200

    performance check

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,25 +13,7 @@
     <div class="background" style="background-image: url('{front}')">
         <div class="water" style="background-image: url('{back}')" />
     </div>
-    <svg>
-        <filter id="turbulence" x="0" y="0" width="100%" height="100%">
-            <feTurbulence
-                id="sea-filter"
-                numOctaves="3"
-                seed="2"
-                baseFrequency="0.02 0.05"
-            />
-            <feDisplacementMap scale="20" in="SourceGraphic" />
-            <animate
-                xlink:href="#sea-filter"
-                attributeName="baseFrequency"
-                dur="300s"
-                keyTimes="0;0.5;1"
-                values="0.02 0.06;0.04 0.08;0.02 0.06"
-                repeatCount="indefinite"
-            />
-        </filter>
-    </svg>
+ 
 </div>
 
 <style>

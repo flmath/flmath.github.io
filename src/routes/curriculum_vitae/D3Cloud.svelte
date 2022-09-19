7545372058c9850f2c9be9339db0d890e01f7ee5
Mathias Green <mathias.green@teliacompany.com>
2022-09-19 07:29:21 +0200

    SEO fix

--- src/routes/curriculum_vitae/D3Cloud.svelte
+++ src/routes/curriculum_vitae/D3Cloud.svelte
@@ -23,8 +23,8 @@
     ];
     const clrs_lenght = clrs.length;
     onMount(async () => {
-        (await import("../../lib/assets/jslibs/d3.v7.js")).default;
-        (await import("../../lib/assets/jslibs/d3.layout.cloud.js")).default;
+        (await import("$lib/assets/jslibs/d3.v7.js")).default;
+        (await import("$lib/assets/jslibs/d3.layout.cloud.js")).default;
         loadwordElements();
     });
     export let width: number;

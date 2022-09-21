7bc59a426af2a3c39196fd0b8e246a1feeecb4bf
Mathias Green <mathias.green@teliacompany.com>
2022-09-21 22:16:27 +0200

    404 message added

--- src/lib/LogoElement.svelte
+++ src/lib/LogoElement.svelte
@@ -1,57 +1,58 @@
 <script lang="ts">
-
-	import { SpinOptions } from "./SpinOptions";
+  import { SpinOptions } from "./SpinOptions";
   import Penrose8 from "./svg/Penrose8.svelte";
-  import { fade } from 'svelte/transition';
-  import { sineOut } from 'svelte/easing';
-  import { page } from '$app/stores';
-  
+  import { fade } from "svelte/transition";
+  import { sineOut } from "svelte/easing";
+  import { page } from "$app/stores";
+
   let active: string = "";
   let activeText: string = "";
 
-  function spin(node: HTMLElement, spin : SpinOptions) {
+  function spin(node: HTMLElement, spin: SpinOptions) {
     let duration = spin.duration;
     let delay = spin.delay;
     return {
-			duration, 
-      delay,     
-			css: (t: number) => {
+      duration,
+      delay,
+      css: (t: number) => {
         const eased = sineOut(t);
-				return `transform: rotate(${spin.rotations * eased * 360}deg);`
-			}
-		};
-	}
-
-  function spinBack(node: HTMLElement, spinOpt :SpinOptions) {
-    return spin(node, new SpinOptions(spinOpt.duration, spinOpt.delay, -3)); 
+        return `transform: rotate(${spin.rotations * eased * 360}deg);`;
+      },
+    };
   }
-  function spinForward(node: HTMLElement, spinOpt : SpinOptions){
-    return spin(node, new SpinOptions(spinOpt.duration, spinOpt.delay, 3)); 
+
+  function spinBack(node: HTMLElement, spinOpt: SpinOptions) {
+    return spin(node, new SpinOptions(spinOpt.duration, spinOpt.delay, -3));
+  }
+  function spinForward(node: HTMLElement, spinOpt: SpinOptions) {
+    return spin(node, new SpinOptions(spinOpt.duration, spinOpt.delay, 3));
   }
   export function handleMouseOver() {
     active = " active ";
     activeText = " active ";
-    
   }
   export function handleMouseOut() {
     activeText = "";
-    if ($page.routeId !== "")
-    active = "";
+    if ($page.routeId !== "") active = "";
   }
 </script>
 
 <li class={"logo" + active}>
-      <a href="/" class="nav-link">
-      {#if (active === "" )} 
-      <span class="button" in:spinBack="{new SpinOptions(1000, 0)}" > <Penrose8 /></span>    
-      {:else }
-      <span class="button" in:spinForward="{new SpinOptions(1000, 0)}" > <Penrose8 /></span>   
-      {/if}
-    {#if (activeText !== "" )} 
-      <span class="link-text logo-text" 
-        in:fade={{duration: 300, delay: 200}}>
-        <slot name="text">Missing</slot></span>
-    {/if}  
+  <a href="/" class="nav-link">
+    {#if active === ""}
+      <span class="button" in:spinBack={new SpinOptions(1000, 0)}>
+        <Penrose8 /></span
+      >
+    {:else}
+      <span class="button" in:spinForward={new SpinOptions(1000, 0)}>
+        <Penrose8 /></span
+      >
+    {/if}
+    {#if activeText !== ""}
+      <span class="link-text logo-text" in:fade={{ duration: 300, delay: 200 }}>
+        <slot name="text">Missing</slot></span
+      >
+    {/if}
   </a>
 </li>
 
@@ -62,15 +63,14 @@
     height: 5rem;
     color: var(--text-primary);
     text-decoration: none;
-    background: none;    
+    background: none;
     filter: grayscale(100%) opacity(0.7);
     transition: var(--transition-speed);
-
   }
   .active .nav-link {
     filter: grayscale(0%) opacity(1);
     color: var(--text-secondary);
-    margin-left: 0rem; 
+    margin-left: 0rem;
   }
   .logo {
     font-weight: bold;
@@ -82,9 +82,8 @@
     color: var(--text-secondary);
     background: none;
     font-size: 2rem;
-    letter-spacing: 0.3ch;    
+    letter-spacing: 0.3ch;
     width: 100%;
-
   }
   /* Small screens */
   @media only screen and (max-width: 600px) {
@@ -108,20 +107,18 @@
   }
   /* Large screens */
   @media only screen and (min-width: 600px) {
-
-    .nav-link .link-text 
-      {
-    display: none; 
-    margin-left: 1rem;
-  }
-  .nav-link .button {   
-    height: 5rem; 
-    margin-top: 1rem;
-    margin-left: 1rem;    
-  }
-  .active .nav-link .link-text {
+    .nav-link .link-text {
+      display: none;
       margin-left: 1rem;
-       display: inline;   
-     }
+    }
+    .nav-link .button {
+      height: 5rem;
+      margin-top: 1rem;
+      margin-left: 1rem;
+    }
+    .active .nav-link .link-text {
+      margin-left: 2rem;
+      display: inline;
+    }
   }
 </style>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+</script>
+
+<a href="/">
+    <div class="glass container">
+        <br />
+        <h1>{$page.status}</h1>
+        <h1>{$page.error.message}</h1>
+    </div>
+</a>
+
+<style>
+    /* @import url(//db.onlinewebfonts.com/c/20143c892228cf6a5a1510b56d583dca?family=Exocet); */
+    .container {
+        font-family: Creepster;
+        color: rgb(0, 89, 255);
+
+        width: 100%;
+        height: 100vh;
+        padding: 0;
+        margin: 0;
+        overflow: clip;
+        align-items: center;
+        text-align: center;
+        /* background: linear-gradient(  167deg,rgba(50, 0, 0, 0.85) 5%, rgba(0, 0, 50, 0.95) 46%,   rgba(0, 0, 0, 1) 100% );  */
+    }
+
+    h1 {
+        color: red;
+        font-family: Creepster;
+    }
+    a {
+        font-family: Creepster;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        font-size: 2rem;
+        text-align: center;
+        letter-spacing: 0.3ch;
+        text-decoration: none;
+        color: var(--text-primary);
+        height: 100%;
+        overflow: clip;
+    }
+    @font-face {
+        font-family: "Creepster";
+        src: url("$lib/assets/fonts/Creepster-Regular.ttf");
+        font-style: normal;
+        font-weight: 400;
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6,
+            U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193,
+            U+2212, U+2215, U+FEFF, U+FFFD;
+    }
+</style>

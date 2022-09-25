03d992a2cf521d180890e1984906375ece71683e
Mathias Green <mathias.green@teliacompany.com>
2022-09-26 00:59:09 +0200

    css fix small screen

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -3,7 +3,7 @@
     import ListItems from "./ListItems.svelte";
 </script>
 
-<div>
+<div class="containO">
     <div class="containS">
         <Search />
     </div>
@@ -13,6 +13,17 @@
 </div>
 
 <style>
+    .containO {
+        background: rgba(255, 255, 255, 0.83);
+        /*based on  https://github.com/Yuvrajchandra/CSS-Background-Patterns/tree/main/1.%20The%20Black%20Hexagons with MIT license*/
+        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='92' height='162' viewBox='0 0 28 49'%3E%3Cg fill-rule='evenodd' %3E%3Cg id='hexagons' fill='%239C92AC' fill-opacity='0.08' fill-rule='nonzero'%3E%3Cpath d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.9v12.7l10.99 6.34 11-6.35V17.9l-11-6.34L3 17.9zM0 15l12.98-7.5V0h-2v6.35L0 12.69v2.3zm0 18.5L12.98 41v8h-2v-6.85L0 35.81v-2.3zM15 0v7.5L27.99 15H28v-2.31h-.01L17 6.35V0h-2zm0 49v-8l12.99-7.5H28v2.31h-.01L17 42.15V49h-2z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E ");
+        background-attachment: fixed;
+
+        background-position:center;
+        padding: 0;
+        margin: 0;
+    }
+
     .containS {
         z-index: 6;
         position: fixed;
@@ -27,9 +38,12 @@
         backdrop-filter: blur(6.4px);
         -webkit-backdrop-filter: blur(6.4px);
         border: 1px solid rgba(255, 255, 255, 0.4);
+        
     }
     .contain {
-        position: relative;
+        width: 100%;
+        position:relative;
         display: contents;
+  
     }
 </style>

143d8cb38ce74457e279f80b128b44ab7a1f4051
Mathias Green <mathias.green@teliacompany.com>
2022-09-26 05:53:14 +0200

    try ssr prerender

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,6 @@
 export const csr = true;
 export const prerender = false;
-export const ssr = false;
+export const ssr = true;
 // prerender: {
 //     // This can be false if you're using a fallback (i.e. SPA mode)
 //     default: false

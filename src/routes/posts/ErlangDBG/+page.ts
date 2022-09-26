a90ce492897846c9f6ba0181b46f4ffb1e8b9829
Mathias Green <mathias.green@teliacompany.com>
2022-09-26 06:52:42 +0200

    csr ssr tryout

---
+++ src/routes/posts/ErlangDBG/+page.ts
@@ -0,0 +1,7 @@
+export const csr = false;
+export const prerender = false;
+export const ssr = true;
+// prerender: {
+//     // This can be false if you're using a fallback (i.e. SPA mode)
+//     default: false
+//   }

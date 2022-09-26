42304923d8a101519f27d73b00678ae7b8b68471
Mathias Green <mathias.green@teliacompany.com>
2022-09-26 20:24:05 +0200

    error /manifest.json fixed

--- src/routes/posts/jupyter/MatricesInErlang/+page.ts
+++
@@ -1,7 +0,0 @@
-export const csr = true;
-export const prerender = false;
-export const ssr = false;
-// prerender: {
-//     // This can be false if you're using a fallback (i.e. SPA mode)
-//     default: false
-//   }

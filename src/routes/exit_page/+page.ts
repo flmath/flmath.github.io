98541c73eb2c01c4b92935d26497bfc13ac38f9f
Mathias Green <mathias.green@teliacompany.com>
2022-09-26 06:29:32 +0200

    prerenders

---
+++ src/routes/exit_page/+page.ts
@@ -0,0 +1,7 @@
+export const csr = true;
+export const prerender = true;
+export const ssr = false;
+// prerender: {
+//     // This can be false if you're using a fallback (i.e. SPA mode)
+//     default: false
+//   }

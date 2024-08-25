667dc3290a8f563ad48ffe497714ba373c043d1c
flmath <flmath@localhost.localdomain>
2024-08-26 00:13:41 +0200

    new jupyter notebooks pipeline

--- src/routes/posts/PostList.ts
+++ src/routes/posts/PostList.ts
@@ -12,12 +12,13 @@ class Item {
 
 export const term = writable('');
 export const items = writable([
-    new Item("Asymetric Cryptography with Openssl", "openssl mathematics crypto ecies rsa elliptic curves", "jupyter/AsymetricCrypto"),  
-    new Item("Breaking RSA Certs with Openssl", "openssl rsa crypto", "jupyter/AsymetricBreakCerts"),
+    new Item("Asymetric Cryptography with Openssl", "openssl mathematics crypto ecies rsa elliptic curves", "jupyter/asymetric_break_certs"),  
+    new Item("Breaking RSA Certs with Openssl", "openssl rsa crypto", "jupyter/asymetric_break_certs"),
     new Item("Erlang dbg use scenarios", "erlang otp dbg debugger programming tracing trace", "ErlangDBG"),
-    new Item("Every answer is right if the question is wrong.", "overfitting interpolation puzzle solutions bias mathematics", "jupyter/Interpolation"),
-    new Item("Matrices in Erlang: Growth Projections ", "growth projections erlang efficiency jupyter mathematics", "jupyter/GrowthProjections"),
-    new Item("Empirical Growth Testing", "growth projections efficiency jupyter mathematics", "jupyter/MatricesInErlang")      
+    new Item("Every answer is right if the question is wrong.", "overfitting interpolation puzzle solutions bias mathematics", "jupyter/interpolation"),
+    new Item("Matrices in Erlang: Growth Projections ", "growth projections erlang efficiency jupyter mathematics", "jupyter/growth_projections"),
+    new Item("Matrices in Erlang: Implementation", "erlang efficiency jupyter mathematics", "jupyter/matrix_implementations_pt1"),
+    new Item("Empirical Growth Testing", "growth projections efficiency jupyter mathematics", "jupyter/Empirical_growth_testing")      
 ]);
 
 export const filtered = derived(

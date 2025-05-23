3bd9db26ac7fed34ebb391e130a922b65f63acc4
Mathias Green <fulllethalmathematic@gmail.com>
2025-05-23 08:59:59 +0200

    Remove particles configuration and related components from exit page for performance optimization

--- src/routes/exit_page/+page.svelte
+++ src/routes/exit_page/+page.svelte
@@ -1,43 +1,18 @@
 <script lang="ts">
-    import Particles from "svelte-particles";
-    import { loadFull } from "tsparticles";
-    import particlesConfig from "$lib/assets/json/particles.json";
+ 
 
-    particlesConfig.background.color.value = "var(--bg-primary)";
+   
 
-    let onParticlesLoaded = (event : Event) => {
-        const particlesContainer = event.detail.particles;
-        // you can use particlesContainer to call all the Container class
-        // (from the core library) methods like play, pause, refresh, start, stop
-    };
 
-    let particlesInit = async (engine) => {
-        // you can use main to customize the tsParticles instance adding presets or custom shapes
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        await loadFull(engine);
-    };
 </script>
 
 <a href="/posts">
     <div>
-        It was planed as the main page, but it is to resource heavy. Bad
-        performance,bad SEO.
+
+
     </div>
 </a>
 <main>
-    {#await particlesInit}
-        " "
-    {:then res}
-        <Particles
-            id="tsparticles"
-            options={particlesConfig}
-            on:particlesLoaded={onParticlesLoaded}
-            {particlesInit}
-        />
-    {:catch error}
-        <p style="color: red">{error.message}</p>
-    {/await}
 </main>
 
 <style>
